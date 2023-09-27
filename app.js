3e1c3e40a040c0e202a177d50188e44290d3a114
AhmedKhilla <ah.khilla97@gmail.com>
2023-09-27 21:06:03 +0200

    last modifications before delivery

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 'use strict';
-//! New Code and new idea !!
+//! Declarations
 const EtS = document.querySelector('#EtS');
 const DtS = document.querySelector('#DtS');
 const StD = document.querySelector('#StD');
+const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'AC', 0, ''];
+const operators = ['+', '-', '*', '/'];
 
+const inputDisplay = document.querySelector('.input-display');
+const equalOperatorButton = document.querySelector('.equal-operator');
+let firstInput;
+let operatorValue;
+
+//! create Euro to Shekels Button
 const sIcon = document.createElement('i');
 sIcon.classList.add('fa-solid');
 sIcon.classList.add('fa-shekel-sign');
@@ -14,23 +22,18 @@ const eIcon = document.createElement('i');
 eIcon.classList.add('fa-solid');
 eIcon.classList.add('fa-euro-sign');
 
-const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'AC', 0, ''];
-const operators = ['+', '-', '*', '/'];
-
-const inputDisplay = document.querySelector('.input-display');
-const equalOperatorButton = document.querySelector('.equal-operator');
-let firstInput;
-let operatorValue;
-
 const createNumberButtons = () => {
   const numbersContainer = document.querySelector('.numbers-container');
+
   numbersArray.forEach(number => {
     const button = document.createElement('button');
     numbersContainer.appendChild(button);
     button.textContent = number;
+
     if (button.textContent === 'AC') {
       button.classList.add('clear-button');
     }
+
     if (button.textContent === '') {
       button.classList.add('StE');
       button.appendChild(sIcon);
@@ -38,7 +41,7 @@ const createNumberButtons = () => {
       button.appendChild(eIcon);
     }
 
-    button.addEventListener('click', () => {
+    const displayNumButtons = function () {
       if (operators.includes(inputDisplay.textContent[0])) {
         inputDisplay.textContent = '';
       }
@@ -47,47 +50,44 @@ const createNumberButtons = () => {
           +inputDisplay.textContent === 0
             ? number
             : inputDisplay.textContent + number;
+    };
 
-      const StE = document.querySelector('.StE');
-      const clearButton = document.querySelector('.clear-button');
-      clearButton.addEventListener('click', () => {
-        inputDisplay.textContent = 0;
-      });
-      StE.addEventListener('click', () => {
-        firstInput = +inputDisplay.textContent;
-        inputDisplay.textContent = firstInput * 0.24;
-      });
-    });
-
+    button.addEventListener('click', displayNumButtons);
     button.removeEventListener('click', () => {
-      if (operators.includes(inputDisplay.textContent[0])) {
-        inputDisplay.textContent = '';
-      }
-
-      inputDisplay.textContent =
-        +inputDisplay.textContent === 0
-          ? number
-          : inputDisplay.textContent + number;
+      displayNumButtons;
     });
+  });
+
+  const StE = document.querySelector('.StE');
+  const clearButton = document.querySelector('.clear-button');
+
+  clearButton.addEventListener('click', () => {
+    inputDisplay.textContent = 0;
+  });
+  StE.addEventListener('click', () => {
+    firstInput = +inputDisplay.textContent;
+    inputDisplay.textContent = firstInput * 0.24;
   });
 };
 
 const createOperatorsButtons = () => {
   const operatorsContainer = document.querySelector('.operators-container');
+
   operators.forEach(operator => {
     const operatorButton = document.createElement('button');
     operatorsContainer.appendChild(operatorButton);
     operatorButton.textContent = operator;
-    operatorButton.addEventListener('click', () => {
+
+    const displayOpButtons = function () {
       firstInput = inputDisplay.textContent;
       inputDisplay.textContent = operator;
       operatorValue = operator;
-    });
+    };
+
+    operatorButton.addEventListener('click', displayOpButtons);
 
     operatorButton.removeEventListener('click', () => {
-      firstInput = inputDisplay.textContent;
-      inputDisplay.textContent = operator;
-      operatorValue = operator;
+      displayOpButtons;
     });
   });
 };
